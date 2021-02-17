acc3c0ee36eec2453642868591e36f7dd9a62c46
Cyrille Le Clerc <cyrille.leclerc@elastic.co>
2021-02-17 17:42:05 +0100

    Bump OpenTelemetry java 0.15 to 0.16 and collector 0.19 to 0.20

--- anti-fraud-java/run-anti-fraud.sh
+++ anti-fraud-java/run-anti-fraud.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -x
 
-export OPEN_TELEMETRY_AGENT_VERSION=0.15.0
+export OPEN_TELEMETRY_AGENT_VERSION=0.16.1
 
 ##########################################################################################
 # PARENT DIRECTORY
