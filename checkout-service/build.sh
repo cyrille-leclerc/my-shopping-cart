5c0b363a2bab09caa463f4ab5f94dcc5f4cd980a
Cyrille Le Clerc <cyrille.leclerc@elastic.co>
2022-04-11 14:11:46 +0200

    Cleanup build process

--- checkout-service/build.sh
+++ checkout-service/build.sh
@@ -4,7 +4,6 @@ if [ -r "setenv.sh" ]; then
   . "setenv.sh"
 fi
 
-mvn -X -Dmaven.ext.class.path=/Users/cyrilleleclerc/git/opentelemetry/opentelemetry-java-contrib/maven-extension/build/libs/opentelemetry-maven-extension-1.10.0-SNAPSHOT.jar \
-    snyk:test snyk:monitor jib:build deploy
+mvn jib:build deploy
 
 # com.google.cloud.tools:jib-maven-plugin:build
