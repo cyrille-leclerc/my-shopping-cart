557deac1a1206e48102022cc6bb6d9e10ce194ec
Cyrille Le Clerc <cyrille.leclerc@grafana.com>
2023-08-03 10:01:38 +0200

    Downgrade Spring Boot to 2.x on shipping to get the jaeger/opentracing instrumentation to work again

--- shipping/src/main/java/com/mycompany/shipping/ShippingController.java
+++ shipping/src/main/java/com/mycompany/shipping/ShippingController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
