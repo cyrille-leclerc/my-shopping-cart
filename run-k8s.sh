e95f0ee7c2f5f557aa5bfe31d802c3140a7c620f
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-05-22 14:17:19 +0200

    add k8s manifests

--- run-k8s.sh
+++ run-k8s.sh
@@ -9,4 +9,7 @@ k3d cluster create my-shopping-cart || true
 k3d image import -c my-shopping-cart checkout-service:1.0-SNAPSHOT fraud-detection:1.0-SNAPSHOT frontend:1.0-SNAPSHOT
 
 # apply all the manifests from the k8s directory
+# todo: the services should be created in the correct order
 kubectl apply -f k8s/
+
+kubectl port-forward service/frontend 8080:8080
