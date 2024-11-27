bdc0fbea609f53a4e307520b0e4e479b522ee37f
Cyrille Le Clerc <cyrille.leclerc@grafana.com>
2024-11-27 14:41:24 +0100

    Demo o11y for a multi tenant application

--- otel-javaagent-extensions/src/main/java/com/mycompany/opentelemetry/BaggageLogRecordProcessor.java
+++ otel-javaagent-extensions/src/main/java/com/mycompany/opentelemetry/BaggageLogRecordProcessor.java
@@ -5,26 +5,34 @@ import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.sdk.logs.LogRecordProcessor;
 import io.opentelemetry.sdk.logs.ReadWriteLogRecord;
-import io.opentelemetry.sdk.logs.data.LogRecordData;
 
+import java.util.function.Predicate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Set {@link Baggage} entries as attributes on the LogRecord
+ * <p>
+ * Similar to <a href="https://github.com/open-telemetry/opentelemetry-java-contrib/blob/v1.41.0/baggage-processor/src/main/java/io/opentelemetry/contrib/baggage/processor/BaggageSpanProcessor.java">BaggageSpanProcessor</a>.
+ */
 public class BaggageLogRecordProcessor implements LogRecordProcessor {
 
     private final Logger logger = Logger.getLogger(getClass().getName());
 
+    final Predicate<String> baggageEntryNameFilter;
+
+    public BaggageLogRecordProcessor() {
+        // TODO in production, implement mechanism to filter baggage entries
+        baggageEntryNameFilter = Predicates.alwaysTrue();
+        logger.log(Level.FINE, () -> "BaggageLogRecordProcessor initialized with baggageEntryNameFilter: " + baggageEntryNameFilter);
+    }
+
     @Override
     public void onEmit(Context context, ReadWriteLogRecord logRecord) {
         Baggage.fromContext(context).forEach((baggageEntryName, baggageEntry) -> {
-            logger.log(Level.FINEST, () -> {
-                LogRecordData logRecordData = logRecord.toLogRecordData();
-                return "logRecord['" +
-                        logRecordData.getInstrumentationScopeInfo().getName() + ": " +
-                        logRecordData.getBodyValue() +
-                        ".attribute[" + baggageEntryName + "]=" + baggageEntry.getValue();
-            });
-            logRecord.setAttribute(AttributeKey.stringKey(baggageEntryName), baggageEntry.getValue());
+            if (baggageEntryNameFilter.test(baggageEntryName)) {
+                logRecord.setAttribute(AttributeKey.stringKey(baggageEntryName), baggageEntry.getValue());
+            }
         });
     }
 }
--- otel-javaagent-extensions/src/main/java/com/mycompany/opentelemetry/BaggageSpanProcessor.java
+++ otel-javaagent-extensions/src/main/java/com/mycompany/opentelemetry/BaggageSpanProcessor.java
@@ -1,17 +1,36 @@
 package com.mycompany.opentelemetry;
 
 import io.opentelemetry.api.baggage.Baggage;
-import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.sdk.trace.ReadWriteSpan;
 import io.opentelemetry.sdk.trace.ReadableSpan;
 import io.opentelemetry.sdk.trace.SpanProcessor;
 
+import java.util.function.Predicate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Set {@link Baggage} entries as attributes on the {@link io.opentelemetry.api.trace.Span}.
+ * <p>
+ * Similar to <a href="https://github.com/open-telemetry/opentelemetry-java-contrib/blob/v1.41.0/baggage-processor/src/main/java/io/opentelemetry/contrib/baggage/processor/BaggageSpanProcessor.java">BaggageSpanProcessor</a>.
+ */
 public class BaggageSpanProcessor implements SpanProcessor {
+
     private final Logger logger = Logger.getLogger(getClass().getName());
+
+    final Predicate<String> baggageEntryNameFilter;
+
+    public BaggageSpanProcessor() {
+        // TODO in production, implement mechanism to filter baggage entries
+        baggageEntryNameFilter = Predicates.alwaysTrue();
+        logger.log(Level.FINE, () -> "BaggageSpanProcessor initialized with baggageEntryNameFilter: " + baggageEntryNameFilter);
+    }
+
+    @Override
+    public boolean isStartRequired() {
+        return true;
+    }
 
     /**
      * Set {@link Baggage} entries as attributes on the Span just after {@link io.opentelemetry.api.trace.Span} creation
@@ -23,27 +42,19 @@ public class BaggageSpanProcessor implements SpanProcessor {
     @Override
     public void onStart(Context context, ReadWriteSpan span) {
         Baggage.fromContext(context).forEach((baggageEntryName, baggageEntry) -> {
-            logger.log(Level.FINEST, () -> "Span['" +
-                    span.getName() + "' / " + span.getKind() + ", " +
-                    "spanId=" + span.getSpanContext().getSpanId() + ", " +
-                    "traceId=" + span.getSpanContext().getTraceId() + "]" +
-                    ".attribute[" + baggageEntryName + "]=" + baggageEntry.getValue());
-            span.setAttribute(baggageEntryName, baggageEntry.getValue());
+            if (baggageEntryNameFilter.test(baggageEntryName)) {
+                span.setAttribute(baggageEntryName, baggageEntry.getValue());
+            }
         });
-    }
-
-    @Override
-    public boolean isStartRequired() {
-        return true;
-    }
-
-    @Override
-    public void onEnd(ReadableSpan span) {
-
     }
 
     @Override
     public boolean isEndRequired() {
         return false;
+    }
+
+    @Override
+    public void onEnd(ReadableSpan span) {
+
     }
 }
