561054a64a4a5688d5b2852446c80bc4c2c14fa0
Cyrille Le Clerc <cyrille.leclerc@grafana.com>
2025-02-12 13:50:46 +0100

    Cleanup

--- checkout/src/main/java/com/mycompany/checkout/CheckoutServiceServer.java
+++ checkout/src/main/java/com/mycompany/checkout/CheckoutServiceServer.java
@@ -2,17 +2,10 @@ package com.mycompany.checkout;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
-import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.Duration;
 import java.util.Optional;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -74,71 +67,4 @@ public class CheckoutServiceServer {
         }));
     }
 
-    static class CheckoutServiceImpl extends CheckoutServiceGrpc.CheckoutServiceImplBase {
-
-        private final Logger logger = LoggerFactory.getLogger(getClass());
-        final HttpClient httpClient;
-
-        final URI shippingServiceUrl;
-
-        CheckoutServiceImpl(String shippingServiceUrl) {
-            this.httpClient = HttpClient.newBuilder().build();
-            String separator = shippingServiceUrl.endsWith("/") ? "" : "/";
-            this.shippingServiceUrl = URI.create(shippingServiceUrl + separator + "shipOrder");
-        }
-
-        @Override
-        public void placeOrder(PlaceOrderRequest placeOrderRequest, StreamObserver<PlaceOrderReply> responseObserver) {
-            final int millis = 25 + RANDOM.nextInt(50);
-            try {
-                Thread.sleep(millis);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            if (RANDOM.nextInt(10) == 0) {
-                logger.atWarn()
-                        .addKeyValue("customerId", placeOrderRequest.getName())
-                        .setCause(new RuntimeException("random failure"))
-                        .log("internal-error");
-            }
-            HttpRequest shippingRequest = HttpRequest.newBuilder()
-                    .uri(shippingServiceUrl)
-                    .timeout(Duration.ofMinutes(2))
-                    .POST(HttpRequest.BodyPublishers.ofString(placeOrderRequest.getName()))
-                    .build();
-
-            String shippingResponse;
-            try {
-                HttpResponse<String> shippingResponseObject = this.httpClient.send(shippingRequest, HttpResponse.BodyHandlers.ofString());
-                shippingResponse = shippingResponseObject.body();
-            } catch (IOException | InterruptedException e) {
-                shippingResponse = "Exception invoking " + shippingRequest + " - " + e;
-                logger.error(shippingResponse);
-                logger.atError()
-                        .addKeyValue("customerId", placeOrderRequest.getName())
-                        .addKeyValue("shippingSvcResponse", shippingResponse)
-                        .addKeyValue("durationInMillis", millis)
-                        .log("Order shipping failed");
-            }
-
-            MDC.put("customerId", placeOrderRequest.getName());
-            logger.atInfo()
-                    .addKeyValue("outcome", "success")
-                    .addKeyValue("customerId", placeOrderRequest.getName())
-                    .addKeyValue("shippingSvcResponse", shippingResponse)
-                    .addKeyValue("durationInMillis", millis)
-                    .log("placeOrder");
-            logger.info("Order {} successfully placed", "order-"+ RANDOM.nextInt(1_00000));
-
-            if (RANDOM.nextInt(10) == 0) {
-                RuntimeException exception = new RuntimeException("Checkout failure");
-                responseObserver.onError(exception);
-            }
-            // StressTestUtils.incrementProgressBarSuccess();
-            PlaceOrderReply placeOrderReply = PlaceOrderReply.newBuilder().setMessage("Order successfully placed!").build();
-            responseObserver.onNext(placeOrderReply);
-            responseObserver.onCompleted();
-        }
-    }
 }
--- fraud-detection/src/main/java/com/mycompany/antifraud/FraudDetectionController.java
+++ fraud-detection/src/main/java/com/mycompany/antifraud/FraudDetectionController.java
@@ -1,6 +1,5 @@
 package com.mycompany.antifraud;
 
-import com.google.common.math.IntMath;
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.metrics.DoubleHistogram;
@@ -8,6 +7,7 @@ import io.opentelemetry.api.metrics.Meter;
 import io.opentelemetry.api.trace.Span;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.spi.LoggingEventBuilder;
 import org.springframework.jmx.export.annotation.ManagedAttribute;
 import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,7 +32,6 @@ public class FraudDetectionController {
 
     final Logger logger = LoggerFactory.getLogger(getClass());
 
-    int exceptionPercentage = 0; // DISABLED
 
     int averageDurationMillisOnSmallShoppingCarts = 50;
     int averageDurationMillisOnMediumShoppingCarts = 50;
@@ -40,7 +39,11 @@ public class FraudDetectionController {
 
     int fraudPercentageOnSmallShoppingCarts = 0;
     int fraudPercentageOnMediumShoppingCarts = 0;
-    int fraudPercentageOnLargeShoppingCarts = 20;
+    int fraudPercentageOnLargeShoppingCarts = 15;
+
+    int exceptionPercentageOnSmallShoppingCarts = 0;
+    int exceptionPercentageOnMediumShoppingCarts = 0;
+    int exceptionPercentageOnLargeShoppingCarts = 10;
 
     int valueUpperBoundaryDollarsOnSmallShoppingCart = 10;
     int valueUpperBoundaryDollarsOnMediumShoppingCarts = 100;
@@ -68,57 +71,48 @@ public class FraudDetectionController {
 
         int durationOffsetInMillis;
         int fraudPercentage;
+        int exceptionPercentage;
         if (orderValueDollars < valueUpperBoundaryDollarsOnSmallShoppingCart) {
             fraudPercentage = fraudPercentageOnSmallShoppingCarts;
             durationOffsetInMillis = averageDurationMillisOnSmallShoppingCarts;
+            exceptionPercentage = exceptionPercentageOnSmallShoppingCarts;
         } else if (orderValueDollars < valueUpperBoundaryDollarsOnMediumShoppingCarts) {
             fraudPercentage = fraudPercentageOnMediumShoppingCarts;
             durationOffsetInMillis = averageDurationMillisOnMediumShoppingCarts;
+            exceptionPercentage = exceptionPercentageOnMediumShoppingCarts;
         } else {
             fraudPercentage = fraudPercentageOnLargeShoppingCarts;
             durationOffsetInMillis = averageDurationMillisOnLargeShoppingCart;
+            exceptionPercentage = exceptionPercentageOnLargeShoppingCarts;
         }
 
-        int randomDurationInMillis = Math.max(5, new BigDecimal(durationOffsetInMillis).multiply(FIVE_PERCENT).intValue());
-        int checkOrderDurationMillis = durationOffsetInMillis + RANDOM.nextInt(randomDurationInMillis);
+        int checkOrderDurationMillis = durationOffsetInMillis + RANDOM.nextInt(Math.max(5, new BigDecimal(durationOffsetInMillis).multiply(FIVE_PERCENT).intValue()));
         // positive means fraud
         int fraudScore = fraudPercentage - RANDOM.nextInt(100);
         Span.current().setAttribute("fraud_score", fraudScore);
 
-        boolean denied = fraudScore > 0;
         String outcome;
+        LoggingEventBuilder eventBuilder = logger.atInfo();
         try (Connection cnn = dataSource.getConnection()) {
             try (Statement stmt = cnn.createStatement()) {
 
                 stmt.execute("select pg_sleep(0.05)");
                 Thread.sleep(checkOrderDurationMillis);
             }
-            if (exceptionPercentage != 0 && RANDOM.nextInt(IntMath.divide(100, exceptionPercentage, RoundingMode.CEILING)) == 1) {
+            if (exceptionPercentage - RANDOM.nextInt(100) > 0) {
                 throw new RuntimeException("Fraud Detection Processing Exception");
             }
-            outcome = denied ? "denied" : "approved";
-            logger.atInfo()
-                    .addKeyValue("outcome", outcome)
-                    .addKeyValue("orderValue", orderValueDollars)
-                    .addKeyValue("shippingCountry", shippingCountry)
-                    .addKeyValue("customerIpAddress", customerIpAddress)
-                    .addKeyValue("fraudScore", fraudScore)
-                    .log("checkOrder");
-            this.fraudDetectionHistogram.record((int) Math.floor(orderValue), Attributes.of(AttributeKey.stringKey("outcome"), outcome));
-            return outcome;
+            outcome = fraudScore > 0 ? "denied" : "approved";
+
         } catch (SQLException | InterruptedException | RuntimeException e) {
-            this.fraudDetectionHistogram.record((int) Math.floor(orderValue), Attributes.of(AttributeKey.stringKey("outcome"), "error"));
             Span.current().recordException(e);
-            logger.atWarn()
-                    .addKeyValue("outcome", "error")
-                    .addKeyValue("orderValue", orderValueDollars)
-                    .addKeyValue("shippingCountry", shippingCountry)
-                    .addKeyValue("customerIpAddress", customerIpAddress)
-                    .addKeyValue("fraudScore", fraudScore)
-                    .setCause(e)
-                    .log("checkOrder");
-            return "error";
+            eventBuilder = logger.atWarn().setCause(e);
+            outcome = "error";
         }
+        this.fraudDetectionHistogram.record(orderValueDollars, Attributes.of(AttributeKey.stringKey("outcome"), outcome));
+        eventBuilder.log("checkOrder: outcome={}, orderValue={}, shippingCountry={}, customerIpAddress={}, fraudScore={}",
+                outcome, orderValueDollars, shippingCountry, customerIpAddress, fraudScore);
+        return outcome;
     }
 
 
